f0f9e51a2014e9f7ed9310c94a2f976f2617c964
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-03 08:19:34 +0530

    Update colormapping.c

--- colormapping.c
+++ colormapping.c
@@ -1,1 +1,10 @@
+// colormapping.h
+#ifndef COLOR_MAPPING_H
+#define COLOR_MAPPING_H
 
+extern const char* major_color_names[];
+extern const char* minor_color_names[];
+extern int total_major_colors;
+extern int total_minor_colors;
+
+#endif // COLOR_MAPPING_H
