70fd58be6b5e35d45e5aac29643880d6ab410544
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-03 09:24:07 +0530

    Update colorpair.c

--- colorpair.c
+++ colorpair.c
@@ -1,7 +1,19 @@
 
 #include <stdio.h>
-#include "colormapping.h"
 #include "colorpair.h"
+
+
+const char* major_color_names[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+int total_major_colors = sizeof(major_color_names) / sizeof(major_color_names[0]);
+
+const char* minor_color_names[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int total_minor_colors = sizeof(minor_color_names) / sizeof(minor_color_names[0]);
 
 void color_pair_to_string(const ColorPair* color_pair, char* output_buffer) {
     sprintf(output_buffer, "%s %s",
