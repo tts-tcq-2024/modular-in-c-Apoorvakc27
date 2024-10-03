a0ad34f3156ba94d1297704b4025208c80dbe292
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-03 15:21:25 +0530

    Update test_colorpair.c

--- test_colorpair.c
+++ test_colorpair.c
@@ -12,7 +12,7 @@ void test_pair_number_to_color(int pair_number,
     ColorPair color_pair = get_color_from_pair_number(pair_number);
     char color_pair_name[16];  // 16 is enough for buffer size
     color_pair_to_string(&color_pair, color_pair_name);
-    printf("Got pair %s\n", color_pair_name);
+    //printf("Got pair %s\n", color_pair_name);
     assert(color_pair.major_color == expected_major_color);
     assert(color_pair.minor_color == expected_minor_color);
 }
@@ -26,7 +26,7 @@ void test_color_to_pair_number(
     color_pair.major_color = major_color;
     color_pair.minor_color = minor_color;
     int pair_number = get_pair_number_from_color(&color_pair);
-    printf("Got pair number %d\n", pair_number);
+    //printf("Got pair number %d\n", pair_number);
     assert(pair_number == expected_pair_number);
 }
 
@@ -39,7 +39,7 @@ void test_reference_manual(void)
      strcpy( test_pair.cable_minor,"Green");
      cable_pair_mapping_manual(mapping_manual);
     
-      printf("Got pair for ref %d %d\n", test_pair.cable_num,mapping_manual[2].cable_num);
+     // printf("Got pair for ref %d %d\n", test_pair.cable_num,mapping_manual[2].cable_num);
       assert(test_pair.cable_num == mapping_manual[2].cable_num);
     
 }
