443eb7abd3080b254c45d2de0575615dce35054e
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-03 08:21:12 +0530

    Update colorpair.h

--- colorpair.h
+++ colorpair.h
@@ -1,1 +1,17 @@
 
+#ifndef COLOR_PAIR_H
+#define COLOR_PAIR_H
+
+#include "colormapping.h"
+
+typedef struct {
+    enum MajorColor major_color;
+    enum MinorColor minor_color;
+} ColorPair;
+
+void color_pair_to_string(const ColorPair* color_pair, char* output_buffer);
+ColorPair get_color_from_pair_number(int pair_number);
+int get_pair_number_from_color(const ColorPair* color_pair);
+
+#endif // COLOR_PAIR_H
+
