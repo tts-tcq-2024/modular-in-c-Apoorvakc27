4351dd073984a1f561fd3c47d52504e17f001157
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-03 08:21:39 +0530

    Update test_colorpair.h

--- test_colorpair.h
+++ test_colorpair.h
@@ -1,1 +1,11 @@
+// test_colorpair.h
+#ifndef TEST_COLOR_PAIR_H
+#define TEST_COLOR_PAIR_H
+
+#include "colorpair.h"
+
+void test_pair_number_to_color(int pair_number, enum MajorColor expected_major_color, enum MinorColor expected_minor_color);
+void test_color_to_pair_number(enum MajorColor major_color, enum MinorColor minor_color, int expected_pair_number);
+
+#endif // TEST_COLOR_PAIR_H
 
