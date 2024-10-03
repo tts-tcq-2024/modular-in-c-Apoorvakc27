fe07169c65542e2db28e0899ea1e3866256d3ac3
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-03 09:08:20 +0530

    Update Reference_Manual.c

--- Reference_Manual.c
+++ Reference_Manual.c
@@ -2,12 +2,6 @@
 #include "colorpair.h"
 #include "reference_manual.h"
 
-typedef struct
-{
-  int cable_num;
-  char cable_major[10];
-  char cable_minor[10];
-}map;
 
 void cable_pair_mapping_manual(map *map_manual)
 {
