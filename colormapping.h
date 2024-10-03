04a2d6edc10926fbd8c0e96eee51c12a4696a4c0
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-03 09:47:49 +0530

    Update colormapping.h

--- colormapping.h
+++ colormapping.h
@@ -1,13 +1,4 @@
 #ifndef COLORMAPPING_H
 #define COLORMAPPING_H
-const char* major_color_names[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-
-
-const char* minor_color_names[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
 
 #endif
