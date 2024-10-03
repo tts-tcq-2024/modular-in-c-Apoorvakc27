393a7037747d72a26176cced331bcc8b9b7dbc10
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-03 09:45:07 +0530

    Update colormapping.h

--- colormapping.h
+++ colormapping.h
@@ -4,11 +4,10 @@ const char* major_color_names[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
-int total_major_colors = sizeof(major_color_names) / sizeof(major_color_names[0]);
+
 
 const char* minor_color_names[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
-int total_minor_colors = sizeof(minor_color_names) / sizeof(minor_color_names[0]);
 #endif
